141c527e6535b5201455868a05949d4dc2f05482
Daniel Crawford <aizuchi0@icloud.com>
2020-05-04 18:46:15 +0000

    Now referring to SmartFusion headers in their own tree.

--- src/platform/arm-smartfusion/targets.mk
+++ src/platform/arm-smartfusion/targets.mk
@@ -27,10 +27,17 @@ TCFLAGS += -ffunction-sections -fdata-sections
 TLFLAGS += --gc-sections
 
 VPATH += $(SRC)/platform/arm-smartfusion
+VPATH += $(SRC)/platform/arm-smartfusion/smartfusion/drivers/mss_uart
+VPATH += $(SRC)/platform/arm-smartfusion/smartfusion/drivers/mss_spi/drivers/mss_spi/
 VPATH += $(SRC)/lib
 
 # This directory, including board information
 INCS += -I$(SRC)/platform/arm-smartfusion
+INCS += -I$(SRC)/platform/arm-smartfusion/smartfusion/drivers/mss_uart/
+INCS += -I$(SRC)/platform/arm-smartfusion/smartfusion/drivers/mss_timer/
+INCS += -I$(SRC)/platform/arm-smartfusion/smartfusion/drivers/mss_watchdog/drivers/mss_watchdog/
+INCS += -I$(SRC)/platform/arm-smartfusion/smartfusion/drivers/mss_spi/drivers/mss_spi/
+INCS += -I$(SRC)/platform/arm-smartfusion/smartfusion/CMSIS/
 
 include $(SRC)/common.mk
 include $(SRC)/cforth/targets.mk
